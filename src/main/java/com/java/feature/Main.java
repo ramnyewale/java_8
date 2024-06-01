d58bf7ab3723bccef137cfaab2ef86f2a5d35b2e
Ram Yewale <ramn.yewale@gmail.com>
2024-06-01 22:27:03 +0530

    new commit

--- src/main/java/com/java/feature/Main.java
+++ src/main/java/com/java/feature/Main.java
@@ -2,6 +2,8 @@ package com.java.feature;
 
 public class Main {
     public static void main(String[] args) {
+        ApplicationHelper applicationHelper = new ApplicationHelper();
+        applicationHelper.trigger();
         System.out.println("Hello and welcome!");
     }
 }
